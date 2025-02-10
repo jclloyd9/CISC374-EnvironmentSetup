19d1f380a7717b918c7c3da0fabe572c2e17b229
jclloyd9 <jclloyd@udel.edu>
2025-02-10 11:05:16 -0500

    Telling jokes like a professional comedian

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("There are 10 types of people: Those who understand binary, and those who don't");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
